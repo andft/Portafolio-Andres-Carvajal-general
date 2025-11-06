34d255a5a1830906ef16727c1d8dec8e6dd6c8d9
Your Name <andrescarvajal1415@gmail.com>
2025-11-05 21:37:07 -0500

    pokecard2.0

--- portafolio/pokemon/Pokemon/src/App.vue
+++ portafolio/pokemon/Pokemon/src/App.vue
@@ -12,53 +12,103 @@
       <div class="busqueda">
         <div>Inicio</div>
         <div>
-          <input v-model="nombre" type="text" placeholder="Busca tu pokemon" class="input-img" id="pokemon"/>
-          <button @click="buscarPokemon">Buscar</button>
+          <input
+            v-model="nombre"
+            type="text"
+            placeholder="Busca tu pokemon"
+            id="pokemon"
+          />
+   <button @click="buscarPokemon" class="input-img">
+  <img :src="'https://cdn-icons-png.flaticon.com/512/622/622669.png'" :alt="imgLupa" />
+</button>
         </div>
       </div>
     </nav>
 
     <section>
-      <div class="infoPokemon" v-if="pokemonData">
+      <div
+        class="infoPokemon"
+        v-if="pokemonData"
+        :style="{
+          border: '6px solid ' + generarColorFondo(pokemonData.types),
+          borderImage: generarColorFondo(pokemonData.types),
+          borderImageSlice: 1,
+        }"
+      >
         <div class="imagen">
-          <img :src="pokemonData.sprites.other['official-artwork'].front_default" alt="pokemon" />
-          <h2>{{pokemonData.name}}</h2>
+          <div class="habitad" :style="{ 'background-image': `url(${obtenerTipo(pokemonData.types[0].type.name).fondo})` }">
+            <img
+              :src="pokemonData.sprites.other['official-artwork'].front_default"
+              alt="pokemon"
+            />
+          </div>
+          <h2>{{ pokemonData.name }}</h2>
         </div>
         <div class="datos">
-          <div class="">
-            <h3>Tipo:</h3>
-          <div class="tipos" v-for="(tipo, i) in pokemonData.types" :key="i">
-            <div class="tipo">
-              <img :src="obtenerTipo(tipo.type.name)" :alt="tipo.type.name" />
-              <p>{{tipo.type.name}}</p>
+          <h3>Tipo:</h3>
+          <div class="tipos">
+            <div
+              class="tipo"
+              v-for="(tipo, i) in pokemonData.types"
+              :key="i"
+              :style="{
+                backgroundColor: obtenerTipo(tipo.type.name).color,
+              }"
+            >
+              <img
+                :src="obtenerTipo(tipo.type.name).imagen"
+                :alt="tipo.type.name"
+              />
+              <p>{{ tipo.type.name }}</p>
             </div>
           </div>
-          <hr>
+          <hr />
+          <h3>Debilidades:</h3>
           <div class="debilidades">
-            <h3>Debilidades:</h3>
             <div class="tipos">
-              <div class="tipo"  v-for="(debilidad, i) in debilidades" :key="i">
-                <img :src="obtenerTipo(debilidad.name)" alt="debilidad" />
-                <p>{{debilidad.name}}</p>
+              <div
+                class="tipo"
+                v-for="(debilidad, i) in debilidades"
+                :key="i"
+                :style="{
+                  backgroundColor: obtenerTipo(debilidad.name).color,
+                }"
+              >
+                <img
+                  :src="obtenerTipo(debilidad.name).imagen"
+                  alt="debilidad"
+                />
+                <p>{{ debilidad.name }}</p>
               </div>
             </div>
           </div>
-          <hr>
+          <hr />
+          <h3>Datos Fisicos:</h3>
           <div class="info">
-            <h3>Datos Fisicos:</h3>
-            <p>Altura: {{pokemonData.height/10}} m</p>
-            <p>Peso: {{pokemonData.weight/10}} kg</p>
-          </div>
+            <p>Altura: {{ pokemonData.height / 10 }} m</p>
+            <p>Peso: {{ pokemonData.weight / 10 }} kg</p>
           </div>
         </div>
+        <div class="separador"></div>
         <div class="habilidades">
           <h3>Habilidades:</h3>
-          <div v-for="(stat, i) in pokemonData.stats" :key="i" class="barra">
-            <span>{{ stat.stat.name }}: {{ stat.base_stat }}</span>
-            <div class="barra-fondo">
-              <div class="barra-progreso" :style="{ width: stat.base_stat + '%' }"></div>
+          <div class="stat" v-for="(stat, i) in pokemonData.stats" :key="i">
+            <span class="nombre-stat">{{ stat.stat.name }}</span>
+            <div class="barra-animada">
+              <div
+                class="relleno-animado"
+                :style="{
+                  width: stat.base_stat + '%',
+                  background: `linear-gradient(270deg, ${
+                    obtenerTipo(pokemonData.types[0].type.name).color
+                  }, #ffffff, ${
+                    obtenerTipo(pokemonData.types[0].type.name).color
+                  })`,
+                }"
+              ></div>
             </div>
-            </div>
+            <span class="valor">{{ stat.base_stat }}</span>
+          </div>
         </div>
       </div>
     </section>
@@ -82,9 +132,7 @@ async function buscarPokemon() {
   }
 
   try {
-    const resPokemon = await axios.get(
-      `${url}${nombre.value.toLowerCase()}`
-    );
+    const resPokemon = await axios.get(`${url}${nombre.value.toLowerCase()}`);
     pokemonData.value = resPokemon.data;
     error.value = null;
 
@@ -100,50 +148,117 @@ async function buscarPokemon() {
 }
 
 function obtenerTipo(type) {
-  switch (type.toLowerCase()) {
-    case "steel":
-      return "https://images.wikidexcdn.net/mwuploads/wikidex/6/6c/latest/20230128124521/Tipo_acero_icono_EP.svg";
-    case "water":
-      return "https://images.wikidexcdn.net/mwuploads/wikidex/d/d6/latest/20230128124702/Tipo_agua_icono_EP.svg";
-    case "bug":
-      return "https://images.wikidexcdn.net/mwuploads/wikidex/1/1a/latest/20230128124809/Tipo_bicho_icono_EP.svg";
-    case "dragon":
-      return "https://images.wikidexcdn.net/mwuploads/wikidex/1/15/latest/20230128124905/Tipo_drag%C3%B3n_icono_EP.svg";
-    case "electric":
-      return "https://images.wikidexcdn.net/mwuploads/wikidex/8/84/latest/20230128125008/Tipo_el%C3%A9ctrico_icono_EP.svg";
-    case "ghost":
-      return "https://images.wikidexcdn.net/mwuploads/wikidex/3/3d/latest/20230128125103/Tipo_fantasma_icono_EP.svg";
-    case "fire":
-      return "https://images.wikidexcdn.net/mwuploads/wikidex/5/55/latest/20230128125153/Tipo_fuego_icono_EP.svg";
-    case "fairy":
-      return "https://images.wikidexcdn.net/mwuploads/wikidex/b/b7/latest/20230128125233/Tipo_hada_icono_EP.svg";
-    case "ice":
-      return "https://images.wikidexcdn.net/mwuploads/wikidex/a/a6/latest/20230128125423/Tipo_hielo_icono_EP.svg";
-    case "fighting":
-      return "https://images.wikidexcdn.net/mwuploads/wikidex/f/f2/latest/20230128125518/Tipo_lucha_icono_EP.svg";
-    case "normal":
-      return "https://images.wikidexcdn.net/mwuploads/wikidex/c/c3/latest/20230128125621/Tipo_normal_icono_EP.svg";
-    case "grass":
-      return "https://images.wikidexcdn.net/mwuploads/wikidex/e/ed/latest/20230128125654/Tipo_planta_icono_EP.svg";
-    case "psychic":
-      return "https://images.wikidexcdn.net/mwuploads/wikidex/2/22/latest/20230128125735/Tipo_ps%C3%ADquico_icono_EP.svg";
-    case "rock":
-      return "https://images.wikidexcdn.net/mwuploads/wikidex/1/14/latest/20230128125805/Tipo_roca_icono_EP.svg";
-    case "dark":
-      return "https://images.wikidexcdn.net/mwuploads/wikidex/e/e0/latest/20230128132504/Tipo_siniestro_icono_EP.svg";
-    case "ground":
-      return "https://images.wikidexcdn.net/mwuploads/wikidex/c/c8/latest/20230128132625/Tipo_tierra_icono_EP.svg";
-    case "poison":
-      return "https://images.wikidexcdn.net/mwuploads/wikidex/f/fa/latest/20230128132735/Tipo_veneno_icono_EP.svg";
-    case "flying":
-      return "https://images.wikidexcdn.net/mwuploads/wikidex/6/6b/latest/20230128132815/Tipo_volador_icono_EP.svg";
+  const coloresTipo = {
+    steel: "#B7B9D0",
+    water: "#6493EA",
+    bug: "#A7B723",
+    dragon: "#7037FF",
+    electric: "#F9CF30",
+    ghost: "#70559B",
+    fire: "#F57D31",
+    fairy: "#E69EAC",
+    ice: "#9AD6DF",
+    fighting: "#C12239",
+    normal: "#AAA67F",
+    grass: "#74CB48",
+    psychic: "#FB5584",
+    rock: "#B69E31",
+    dark: "#75574C",
+    ground: "#DEC16B",
+    poison: "#A43E9E",
+    flying: "#A891EC",
+    default: "#A8A8A8",
+  };
+
+  const imagenesTipo = {
+    steel:
+      "https://images.wikidexcdn.net/mwuploads/wikidex/6/6c/latest/20230128124521/Tipo_acero_icono_EP.svg",
+    water:
+      "https://images.wikidexcdn.net/mwuploads/wikidex/d/d6/latest/20230128124702/Tipo_agua_icono_EP.svg",
+    bug: "https://images.wikidexcdn.net/mwuploads/wikidex/1/1a/latest/20230128124809/Tipo_bicho_icono_EP.svg",
+    dragon:
+      "https://images.wikidexcdn.net/mwuploads/wikidex/1/15/latest/20230128124905/Tipo_drag%C3%B3n_icono_EP.svg",
+    electric:
+      "https://images.wikidexcdn.net/mwuploads/wikidex/8/84/latest/20230128125008/Tipo_el%C3%A9ctrico_icono_EP.svg",
+    ghost:
+      "https://images.wikidexcdn.net/mwuploads/wikidex/3/3d/latest/20230128125103/Tipo_fantasma_icono_EP.svg",
+    fire: "https://images.wikidexcdn.net/mwuploads/wikidex/5/55/latest/20230128125153/Tipo_fuego_icono_EP.svg",
+    fairy:
+      "https://images.wikidexcdn.net/mwuploads/wikidex/b/b7/latest/20230128125233/Tipo_hada_icono_EP.svg",
+    ice: "https://images.wikidexcdn.net/mwuploads/wikidex/a/a6/latest/20230128125423/Tipo_hielo_icono_EP.svg",
+    fighting:
+      "https://images.wikidexcdn.net/mwuploads/wikidex/f/f2/latest/20230128125518/Tipo_lucha_icono_EP.svg",
+    normal:
+      "https://images.wikidexcdn.net/mwuploads/wikidex/c/c3/latest/20230128125621/Tipo_normal_icono_EP.svg",
+    grass:
+      "https://images.wikidexcdn.net/mwuploads/wikidex/e/ed/latest/20230128125654/Tipo_planta_icono_EP.svg",
+    psychic:
+      "https://images.wikidexcdn.net/mwuploads/wikidex/2/22/latest/20230128125735/Tipo_ps%C3%ADquico_icono_EP.svg",
+    rock: "https://images.wikidexcdn.net/mwuploads/wikidex/1/14/latest/20230128125805/Tipo_roca_icono_EP.svg",
+    dark: "https://images.wikidexcdn.net/mwuploads/wikidex/e/e0/latest/20230128132504/Tipo_siniestro_icono_EP.svg",
+    ground:
+      "https://images.wikidexcdn.net/mwuploads/wikidex/c/c8/latest/20230128132625/Tipo_tierra_icono_EP.svg",
+    poison:
+      "https://images.wikidexcdn.net/mwuploads/wikidex/f/fa/latest/20230128132735/Tipo_veneno_icono_EP.svg",
+    flying:
+      "https://images.wikidexcdn.net/mwuploads/wikidex/6/6b/latest/20230128132815/Tipo_volador_icono_EP.svg",
     default:
-      return "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg"; // default icon
+      "https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg",
+  };
+
+  const fondosTipo = {
+  steel: new URL('./assets/fondos/steel.png', import.meta.url).href,
+  water: new URL('./assets/fondos/water.png', import.meta.url).href,
+  bug: new URL('./assets/fondos/bug.png', import.meta.url).href,
+  dragon: new URL('./assets/fondos/dragon.png', import.meta.url).href,
+  electric: new URL('./assets/fondos/electric.png', import.meta.url).href,
+  ghost: new URL('./assets/fondos/ghost.png', import.meta.url).href,
+  fire: new URL('./assets/fondos/fire.png', import.meta.url).href,
+  fairy: new URL('./assets/fondos/fairy.png', import.meta.url).href,
+  ice: new URL('./assets/fondos/ice.png', import.meta.url).href,
+  fighting: new URL('./assets/fondos/fighting.png', import.meta.url).href,
+  default: new URL('./assets/fondos/normal.png', import.meta.url).href,
+  grass: new URL('./assets/fondos/grass.png', import.meta.url).href,
+  psychic: new URL('./assets/fondos/psychic.png', import.meta.url).href,
+  rock: new URL('./assets/fondos/rock.png', import.meta.url).href,
+  dark: new URL('./assets/fondos/dark.png', import.meta.url).href,
+  ground: new URL('./assets/fondos/ground.png', import.meta.url).href,
+  poison: new URL('./assets/fondos/poison.png', import.meta.url).href,
+  flying: new URL('./assets/fondos/flying.png', import.meta.url).href,
+  default: new URL('./assets/fondos/default.png', import.meta.url).href,
+};
+
+  const tipo = type.toLowerCase();
+
+  return {
+    imagen: imagenesTipo[tipo] || imagenesTipo.default,
+    color: coloresTipo[tipo] || coloresTipo.default,
+    fondo: fondosTipo[tipo] || fondosTipo.default
+  };
+}
+
+function generarColorFondo(types) {
+  const colores = types.map((t) => obtenerTipo(t.type.name).color);
+
+  if (colores.length === 1) {
+    return colores[0];
   }
+
+  const porcentajePaso = 100 / (colores.length - 1);
+  const gradiente = colores
+    .map((color, i) => `${color} ${i * porcentajePaso}%`)
+    .join(", ");
+
+  return `linear-gradient(135deg, ${gradiente})`;
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   margin: 0px;
   padding: 0px;
@@ -169,6 +284,10 @@ nav {
   font-family: Arial, sans-serif;
 }
 
+p {
+  text-transform: capitalize;
+}
+
 h2 {
   margin: 0;
   font-size: 24px;
@@ -188,20 +307,20 @@ h2 {
   font-size: 16px;
 }
 
-.busqueda > div:first-child {
-  border-bottom: 2px solid white;
-  padding-bottom: 5px;
-}
-
 .input-img {
-  background-image: url("https://cdn-icons-png.flaticon.com/512/622/622669.png");
+  width: 30px;
+  height: 30px;
   background-repeat: no-repeat;
   background-position: 10px center;
-  background-size: 18px;
+  background-size: 10px;
   padding-left: 40px;
   padding-top: 8px;
   padding-bottom: 8px;
   padding-right: 15px;
+}
+
+.input-img img{
+  width: 25px;
 }
 
 input {
@@ -224,13 +343,17 @@ section {
 
 .infoPokemon {
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  border: 8px solid #7dd3fc;
-  border-radius: 15px;
+  grid-template-columns: 1fr 1fr 1px 1fr;
+  width: 90%;
   background-color: #3a3a3a;
-  width: 1200px;
-  max-width: 90%;
   overflow: hidden;
+}
+
+.habitad {
+  background-size: cover;
+  background-position: center;
+  border-radius: 16px;
+  box-shadow: 0 4px 15px rgba(0, 0, 0, 0.4);
 }
 
 .imagen {
@@ -244,8 +367,7 @@ section {
 
 .imagen img {
   width: 320px;
-  height: 320px;/* 
-  object-fit: contain; */
+  height: 320px;
 }
 
 .imagen h2 {
@@ -265,13 +387,11 @@ section {
   margin: 30px 0;
   color: white;
   font-family: Arial, sans-serif;
-  text-align: start;
   font-size: 18px;
-  background-color: #3a3a3a;
 }
 
 h3 {
-  margin: 0 0 12px 0;
+  margin: 0;
   font-size: 20px;
   font-weight: bold;
   text-transform: uppercase;
@@ -280,11 +400,11 @@ h3 {
 
 .tipos {
   display: flex;
+  flex-wrap: wrap;
   align-items: center;
   justify-content: center;
-  flex-wrap: wrap;
   gap: 12px;
-  margin-bottom: 5px;
+  width: 30vw;
 }
 
 .tipos p {
@@ -292,23 +412,37 @@ h3 {
   font-size: 18px;
 }
 
-.debilidades {
-  margin-top: 20px;
+.tipo {
+  display: flex;
+  align-items: center;
+  gap: 6px;
+  padding: 6px 10px;
+  border-radius: 8px;
 }
 
-.info {
-  margin-top: 20px;
+.tipo img {
+  width: 32px;
+  height: 32px;
+  border-radius: 30%;
+  object-fit: cover;
 }
 
 .info p {
-  margin: 8px 0;
   font-size: 18px;
+  margin: 3px;
 }
 
 hr {
   border: none;
   border-top: 1px solid #5a5a5a;
-  margin: 5px 0;
+  margin: 0px 10px;
+  height: 1px;
+}
+
+.separador {
+  border-left: 1px solid #5a5a5a;
+  margin: 20px 0;
+  width: 1px;
 }
 
 .habilidades {
@@ -319,40 +453,34 @@ hr {
   color: white;
 }
 
-.barra {
-  width: 250px;
+.stat {
+  display: grid;
+  gap: 3px;
+  width: 25vw;
+  text-transform: capitalize;
 }
 
-.barra-fondo {
-  background-color: #eee;
-  height: 10px;
-  border-radius: 5px;
+.barra-animada {
+  flex: 1;
+  height: 12px;
+  border-radius: 10px;
   overflow: hidden;
+  background: rgba(255, 255, 255, 0.1);
 }
 
-.barra-progreso {
-  height: 10px;
-  background-color: #ffcb05;
-  transition: width 0.3s ease;
+.relleno-animado {
+  height: 100%;
+  background-size: 300% 300%;
+  animation: moverGradiente 2s linear infinite;
+  border-radius: 10px;
 }
 
-.tipos {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 12px;
-}
-
-.tipo {
-  display: flex;
-  align-items: center;
-  gap: 6px;
-  background: #f4f4f4;
-  padding: 6px 10px;
-  border-radius: 8px;
-}
-
-.tipo img {
-  width: 32px;
-  height: 32px;
+@keyframes moverGradiente {
+  0% {
+    background-position: 0% 50%;
+  }
+  100% {
+    background-position: 100% 50%;
+  }
 }
 </style>
