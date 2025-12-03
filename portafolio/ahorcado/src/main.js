604f569443128a620f5e832cc9806c2c5813c8db
andft <andrescarvajal1415@gmail.com>
2025-12-02 19:57:29 -0500

    agregar paginas q faltaban

--- portafolio/ahorcado/src/main.js
+++ portafolio/ahorcado/src/main.js
@@ -1,12 +1,15 @@
 import { createApp } from 'vue'
+//import './style.css'
 import App from './App.vue'
-import { Quasar, Notify } from 'quasar'
-import { router } from "./routes/routes.js"
+import { Quasar } from 'quasar'
+import {router} from "./routes/routes.js"
 
+// Import icon libraries
 import '@quasar/extras/material-icons-sharp/material-icons-sharp.css'
 import '@quasar/extras/fontawesome-v6/fontawesome-v6.css'
-import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/bootstrap-icons/bootstrap-icons.css'
 
+// Import Quasar css
 import 'quasar/src/css/index.sass'
 
 const app = createApp(App)
@@ -14,9 +17,7 @@ const app = createApp(App)
 app.use(router)
 
 app.use(Quasar, {
-  plugins: {
-    Notify
-  }
+  plugins: {}, // import Quasar plugins and add here
 })
 
 app.mount('#app')
